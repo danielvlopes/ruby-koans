e5434a7795f3f6bf0b447415b47bbeb2e053f87e
Daniel Lopes <danielvlopes@gmail.com>
2012-12-17 21:02:33 -0600

    first day progress

--- koans/about_nil.rb
+++ koans/about_nil.rb
@@ -3,13 +3,13 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 class AboutNil < EdgeCase::Koan
 
   def test_nil_is_an_object
-    assert_equal __, nil.is_a?(Object)
+    assert_equal true, nil.is_a?(Object)
   end
 
   def test_nil_has_a_few_methods_defined_on_it
-    assert_equal __, nil.nil?
-    assert_equal __, nil.to_s
-    assert_equal __, nil.inspect
+    assert_equal true, nil.nil?
+    assert_equal "", nil.to_s
+    assert_equal "nil", nil.inspect
 
     # THINK ABOUT IT:
     #
